e17130d9816d0db99603c2eebccab9f439f485d5
Carlos Encinas <carlos-encinas@hotmail.com>
2019-09-23 09:13:20 -0700

    Avances

--- kanji.js
+++ kanji.js
@@ -1,15 +1,17 @@
-const Htmlkanji = document.getElementById('kanji')
+const Htmlkanji = document.getElementById('kanji');
+const Htmlreading = document.getElementById('reading');
 const Htmlmean = document.getElementById('meaning');
 
 const ApiUrl = ('https://kanjiapi.dev/v1/');
 
 getKanjiData = async ()=>{
-    const response = await fetch(`${ApiUrl}kanji/夏`);
+    const response = await fetch(`${ApiUrl}kanji/僕`);
     const Datakanji = await response.json();
     console.log(Datakanji);
-    const {kanji , kun_readings} = Datakanji;
+    const {kanji , kun_readings , meanings} = Datakanji;
     Htmlkanji.innerHTML = kanji; 
-    Htmlmean.innerHTML = kun_readings;
+    Htmlreading.innerHTML = kun_readings;
+    Htmlmean.innerHTML = meanings; 
 }
 
 getKanjiData();
