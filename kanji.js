c16ed8be672952d163729b320264188d949e1223
Carlos Encinas <carlos-encinas@hotmail.com>
2019-09-25 09:02:13 -0700

    jeje

--- kanji.js
+++ kanji.js
@@ -1,11 +1,12 @@
 const Htmlkanji = document.getElementById('kanji');
 const Htmlreading = document.getElementById('reading');
 const Htmlmean = document.getElementById('meaning');
+let Htmlsearch = document.inputEncoding
 
 const ApiUrl = ('https://kanjiapi.dev/v1/');
 
 getKanjiData = async ()=>{
-    const response = await fetch(`${ApiUrl}kanji/僕`);
+    const response = await fetch(`${ApiUrl}kanji/${Htmlsearch}`);
     const Datakanji = await response.json();
     console.log(Datakanji);
     const {kanji , kun_readings , meanings} = Datakanji;
